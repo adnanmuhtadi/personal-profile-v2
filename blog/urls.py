4d469d75521580d259da4316e41020154dfcaf82
Adnan Muhtadi <13140188+adnanmuhtadi@users.noreply.github.com>
2023-02-01 22:35:04 +0000

    blog working

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('', views.all_blogs, name='all_blogs'),
 ]
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,5 +22,5 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
