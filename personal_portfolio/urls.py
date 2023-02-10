298bb79e2ee8827dce89236d4a52a744cdb1e35a
Adnan Muhtadi <13140188+adnanmuhtadi@users.noreply.github.com>
2023-02-10 01:26:04 +0000

    Fix: Static DIR URL fix to settings and urls.py

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path('blog/', include('blog.urls')),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, }),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
