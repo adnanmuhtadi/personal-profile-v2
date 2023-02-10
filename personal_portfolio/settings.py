89350beefc7d43740193a8f3fefdedb704b92a4d
Adnan Muhtadi <13140188+adnanmuhtadi@users.noreply.github.com>
2023-02-10 06:27:49 +0000

    Fix: Small Static fix 4

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -163,13 +163,15 @@ USE_TZ = True
 if 'DEBUG' in os.environ:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 else:
     # Development environment variable
     STATIC_URL = '/static/'
